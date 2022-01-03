d57f7b37e4859e8c51737db223ec1d83f9fffd41
Jaime J Cobo <jaimecobo@outlook.com>
2022-01-03 18:25:46 -0400

    PersonController -- Get person by id

--- Week_6/spring-demo/src/main/java/com/tts/springdemo/controller/PersonController.java
+++ Week_6/spring-demo/src/main/java/com/tts/springdemo/controller/PersonController.java
@@ -3,12 +3,10 @@ package com.tts.springdemo.controller;
 import com.tts.springdemo.model.Person;
 import com.tts.springdemo.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/person")
@@ -27,6 +25,11 @@ public class PersonController {
     @GetMapping("/get/{id}")
     public Person getPersonById(@PathVariable Long id){
         return personRepository.findById(id).orElseThrow();
+    }
+
+    @GetMapping("get/persons")
+    public Optional<Person> getPersons (@RequestParam Long id){
+        return personRepository.findById(id);
     }
 
 }
