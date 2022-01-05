524de815f47de8efcaec780572a9869419737ed1
Jaime J Cobo <jaimecobo@outlook.com>
2022-01-05 00:24:27 -0400

    controller/ProductController.java

--- Week_6/ecommerce/src/main/java/co/jaimecobo/ecommerce/controller/ProductController.java
+++ Week_6/ecommerce/src/main/java/co/jaimecobo/ecommerce/controller/ProductController.java
@@ -1,2 +1,34 @@
-package co.jaimecobo.ecommerce.controller;public class ProductController {
+package co.jaimecobo.ecommerce.controller;
+
+import co.jaimecobo.ecommerce.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
+
+//This is the controller for the product page
+//notice there is a method that allows to create or update a product
+@Controller
+public class ProductController {
+
+    @Autowired
+    ProductService productService;
+
+    @GetMapping("/product/{id}")
+    public String show(@PathVariable int id, Model model){
+        Product product = productService.findById(id);
+        model.addAttribute(product);
+        return "product";
+    }
+
+    @RequestMapping(value = "/product", method = {RequestMethod.POST, RequestMethod.PUT})
+    public String createOrUpdate(@Valid Product product){
+        productService.save(product);
+        return "redirect:/product/" + product.getId();
+    }
 }
