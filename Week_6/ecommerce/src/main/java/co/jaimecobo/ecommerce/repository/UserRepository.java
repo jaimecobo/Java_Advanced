51501574b1ea99df1820f4203e81838408433dae
Jaime J Cobo <jaimecobo@outlook.com>
2022-01-06 00:27:52 -0400

    repository/UserRepository

--- Week_6/ecommerce/src/main/java/co/jaimecobo/ecommerce/repository/UserRepository.java
+++ Week_6/ecommerce/src/main/java/co/jaimecobo/ecommerce/repository/UserRepository.java
@@ -1,2 +1,12 @@
-package co.jaimecobo.ecommerce.repository;public interface userRepository {
+package co.jaimecobo.ecommerce.repository;
+
+import co.jaimecobo.ecommerce.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    User findByUsername(String Username);
+
 }
