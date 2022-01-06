d5dd2fa0ae650d3ab88ed4ba4148f3d59560cebb
Jaime J Cobo <jaimecobo@outlook.com>
2022-01-06 00:26:15 -0400

    model/Product

--- Week_6/ecommerce/src/main/java/co/jaimecobo/ecommerce/model/Product.java
+++ Week_6/ecommerce/src/main/java/co/jaimecobo/ecommerce/model/Product.java
@@ -1,2 +1,30 @@
-package co.jaimecobo.ecommerce.model;public class Product {
+package co.jaimecobo.ecommerce.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private int quantity;
+    private double price;
+
+    private String description;
+    private String name;
+    private String brand;
+    private String category;
+    private String image;
+
 }
