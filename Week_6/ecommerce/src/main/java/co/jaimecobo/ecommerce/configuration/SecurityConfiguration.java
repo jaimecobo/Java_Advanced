b2ba96a875950c2965f6aa2eb132a02ccc46f0d8
Jaime J Cobo <jaimecobo@outlook.com>
2022-01-06 00:24:37 -0400

    Security

--- Week_6/ecommerce/src/main/java/co/jaimecobo/ecommerce/configuration/SecurityConfiguration.java
+++ Week_6/ecommerce/src/main/java/co/jaimecobo/ecommerce/configuration/SecurityConfiguration.java
@@ -1,2 +1,47 @@
-package co.jaimecobo.ecommerce.configuration;public class SecurityConfiguration {
+package co.jaimecobo.ecommerce.configuration;
+
+import co.jaimecobo.ecommerce.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AndRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserService userService;
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests()
+                .antMatchers("/cart")
+                .authenticated()
+                .and().formLogin()
+                .loginPage("/signin")
+                .loginProcessingUrl("/login")
+                .and().logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/signout"))
+                .logoutSuccessUrl("/");
+    }
 }
