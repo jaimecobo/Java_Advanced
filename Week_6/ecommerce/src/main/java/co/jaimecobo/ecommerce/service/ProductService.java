d747f96c080a0bcb5061ef073be698516b3ec039
Jaime J Cobo <jaimecobo@outlook.com>
2022-01-06 00:28:49 -0400

    service/ProductService

--- Week_6/ecommerce/src/main/java/co/jaimecobo/ecommerce/service/ProductService.java
+++ Week_6/ecommerce/src/main/java/co/jaimecobo/ecommerce/service/ProductService.java
@@ -1,2 +1,51 @@
-package co.jaimecobo.ecommerce.service;public class ProductService {
+package co.jaimecobo.ecommerce.service;
+
+import co.jaimecobo.ecommerce.model.Product;
+import co.jaimecobo.ecommerce.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    public Product findById(long id) {
+        return productRepository.findById(id);
+    }
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    public List<String> findDistinctCategories() {
+        return productRepository.findDistinctCategories();
+    }
+
+    public List<String> findDistinctBrands() {
+        return productRepository.findDistinctBrands();
+    }
+
+    public List<Product> findByBrandAndOrCategory(String brand, String category) {
+        if(category == null && brand == null){
+            return productRepository.findAll();
+        } else if (category == null){
+            return productRepository.findByBrand(brand);
+        } else if(brand == null){
+            return productRepository.findByCategory(category);
+        } else { // this "else" is not mandatory but is there to make the logic of the code more detailed
+            return productRepository.findByBrandAndCategory(brand, category);
+        }
+    }
+
+    public void save(Product product) {
+        productRepository.save(product);
+    }
+
+    public void deleteById(long id){
+        productRepository.deleteById(id);
+    }
 }
